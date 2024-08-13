8e7aedad373f101d0685004e8af877604fe35054
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-13 20:12:37 +0900

    fix: kitty config

--- lua/plugins/kitty.lua
+++ lua/plugins/kitty.lua
@@ -1,20 +1,12 @@
 -- only kitty terminal
 -- check here: https://github.com/3rd/image.nvim
-if vim.fn.getenv("KITTY_WINDOW_ID") ~= nil then
+if os.getenv("KITTY_WINDOW_ID") == nil then
   return {}
 end
 
 return {
   {
-    "vhyrro/luarocks.nvim",
-    priority = 1001,
-    opts = {
-      rocks = { "magick" },
-    },
-  },
-  {
     "3rd/image.nvim",
-    dependencies = { "luarocks.nvim" },
     config = function()
       require("image").setup()
     end,
