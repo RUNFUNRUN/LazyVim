c3719bedbc472a7a3a03556143cbb182e3a330aa
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-14 23:18:08 +0900

    fix: add extras packages and delete utils plugins

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,26 +1,12 @@
 return {
-  {
-    "brenoprata10/nvim-highlight-colors",
-    config = function()
-      require("nvim-highlight-colors").setup({
-        enable_tailwind = true,
-      })
-    end,
-  },
+  -- color picker
   {
     "uga-rosa/ccc.nvim",
     config = function()
       require("ccc").setup()
     end,
   },
-  {
-    "vuki656/package-info.nvim",
-    config = function()
-      require("package-info").setup()
-    end,
-  },
   -- discord rich presence
-  -- check here: https://github.com/andweeb/presence.nvim
   {
     "andweeb/presence.nvim",
     config = function()
