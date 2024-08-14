b6370ac4e97c055bb1274efe7b9f8e62fc41a0bc
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-14 21:58:20 +0900

    fix: vscode config
    lualine

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -4,4 +4,19 @@ end
 
 return {
   { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({
+        sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "mode" },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {},
+        },
+      })
+    end,
+  },
 }
