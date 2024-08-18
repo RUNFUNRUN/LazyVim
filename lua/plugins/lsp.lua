9824bad4295891b68f59dc4a7cd9e09e3fd6f6a4
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-19 00:39:15 +0900

    add: java lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,6 @@
 require("utils")
-local emmet = require("plugins.lsp.emmet")
 local inlay_hint = require("plugins.lsp.inlay_hint")
+local java = require("plugins.lsp.java")
+local emmet = require("plugins.lsp.emmet")
 
-return TableConcat(emmet, inlay_hint)
+return TableConcat(inlay_hint, emmet, java)
