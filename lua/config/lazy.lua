93cbd827350a959c57f08b0d40d7e7f5924a1cf0
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-22 23:14:56 +0900

    fix: replace highlight plugin
    to extras util

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -38,6 +38,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.yaml" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.util.octo" },
     { import = "lazyvim.plugins.extras.util.project" },
     { import = "lazyvim.plugins.extras.vscode" },
