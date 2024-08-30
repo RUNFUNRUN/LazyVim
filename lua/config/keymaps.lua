176c7994d06d4ae416e27b0b2bd5840616b975c0
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-31 03:51:50 +0900

    fix: remove require

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-require("utils")
-
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 map("i", "jj", "<Esc>", opts)
