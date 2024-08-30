152591a1c3945854691e11cc8075af798c7c3001
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-08-31 03:50:53 +0900

    feat: denols

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,6 @@
-require("utils")
 local options = require("plugins.lsp.options")
+local typescript = require("plugins.lsp.typescript")
 local java = require("plugins.lsp.java")
 local emmet = require("plugins.lsp.emmet")
 
-return TableConcat(options, emmet, java)
+return TableConcat(options, typescript, emmet, java)
