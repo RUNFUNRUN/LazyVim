126901acf242f488c80ddb232668c29de94df456
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-09-25 18:13:10 +0900

    feat: convert `bun.lockb`

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,3 +7,16 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.mdx",
   command = "set filetype=markdown.mdx",
 })
+
+-- convert `bun.lockb` from bin to text
+vim.api.nvim_create_autocmd("BufReadCmd", {
+  pattern = "bun.lockb",
+  callback = function()
+    local path = vim.fn.expand("%:p")
+    local output = vim.fn.systemlist("bun " .. path)
+    vim.api.nvim_buf_set_lines(0, 0, -1, true, output)
+    vim.opt_local.filetype = "conf"
+    vim.opt_local.readonly = true
+    vim.opt_local.modifiable = false
+  end,
+})
