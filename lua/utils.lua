e40bca66cdcea89f3b801aea2025966ba80304ac
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-03 12:14:50 +0900

    fix: keymaps
    remove enter and shift enter
    fix DeleteLine function
    add DeleteBlock function

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,12 +1,3 @@
-function DeleteLine()
-  local line = vim.api.nvim_get_current_line()
-  if string.match(line, "^%s*$") then
-    vim.api.nvim_command('normal! "_dd')
-  else
-    vim.api.nvim_command("normal! dd")
-  end
-end
-
 function TableConcat(...)
   local result = {}
 
@@ -17,4 +8,34 @@ function TableConcat(...)
   end
 
   return result
+end
+
+function DeleteLine()
+  local line = vim.api.nvim_get_current_line()
+  if string.match(line, "^%s*$") then
+    vim.cmd('normal! "_dd')
+  else
+    vim.cmd("normal! dd")
+  end
+end
+
+function DeleteBlock()
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local lines = vim.fn.getline(start_line, end_line)
+  if type(lines) == "string" then
+    lines = { lines }
+  end
+  local all_blank = true
+  for _, line in ipairs(lines) do
+    if not line:match("^%s*$") then
+      all_blank = false
+      break
+    end
+  end
+  if all_blank then
+    vim.cmd('normal! "_d')
+  else
+    vim.cmd("normal! d")
+  end
 end
