c51079aeb5c3fb2eae7ac700b2fea74f8db12590
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-03 11:23:38 +0900

    fix: im-select.nvim config

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -2,8 +2,21 @@ return {
   {
     "keaising/im-select.nvim",
     init = function()
-      if not vim.env.SSH_TTY then
+      if (not vim.env.SSH_TTY) and vim.env.DISPLAY then
         require("im_select").setup()
+
+        if vim.fn.has("wsl") or vim.fn.has("win32") then
+          vim.api.nvim_create_autocmd("VimLeave", {
+            callback = function()
+              vim.fn.system("im-select.exe 1041")
+            end,
+          })
+          vim.api.nvim_create_autocmd("FocusLost", {
+            callback = function()
+              vim.fn.system("im-select.exe 1041")
+            end,
+          })
+        end
       end
     end,
   },
