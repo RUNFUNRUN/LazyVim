095ca14bb883637fd7502ed053d993a5d6fca7de
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-03 11:05:47 +0900

    fix: disable im-select when ssh

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -2,7 +2,9 @@ return {
   {
     "keaising/im-select.nvim",
     init = function()
-      require("im_select").setup()
+      if not vim.env.SSH_TTY then
+        require("im_select").setup()
+      end
     end,
   },
 }
