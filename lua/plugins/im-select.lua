665b392bac17478368d85c2c30b9ff7e3067f589
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-06 03:41:06 +0900

    fix: remove SSH_TTY

--- lua/plugins/im-select.lua
+++ lua/plugins/im-select.lua
@@ -2,7 +2,7 @@ return {
   {
     "keaising/im-select.nvim",
     init = function()
-      if (not vim.env.SSH_TTY) and vim.env.DISPLAY then
+      if vim.env.DISPLAY then
         require("im_select").setup()
 
         if vim.fn.has("wsl") or vim.fn.has("win32") then
