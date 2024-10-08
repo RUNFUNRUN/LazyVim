7ee7e9a3f2ada0ac87cb62e10c9c25709abe453d
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-08 11:13:03 +0900

    fix: add C-l keymap to insert mode and refactor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,12 +3,51 @@
 -- Add any additional keymaps here
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
-map("i", "jj", "<Esc>", opts)
-map("x", "p", "P", opts)
-map("x", "P", "p", opts)
+
 map("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>", opts)
 map("n", "+", "<C-a>", opts)
 map("n", "-", "<C-x>", opts)
 map("n", "<C-a>", "gg<S-v>G", opts)
-map("n", "dd", "<Cmd>lua DeleteLine()<CR>", opts)
-map("x", "d", "<Cmd>lua DeleteBlock()<CR>", opts)
+-- don't store a blank line in registers
+map("n", "dd", function()
+  local line = vim.api.nvim_get_current_line()
+  if string.match(line, "^%s*$") then
+    vim.cmd('normal! "_dd')
+  else
+    vim.cmd("normal! dd")
+  end
+end, opts)
+
+map("i", "jj", "<Esc>", opts)
+-- converts the last word entered to uppercase
+map("i", "<C-l>", function()
+  local line = vim.fn.getline(".")
+  local col = vim.fn.getpos(".")[3]
+  local substring = line:sub(1, col - 1)
+  local result = vim.fn.matchstr(substring, [[\v<(\k(<)@!)*$]])
+  return "<C-w>" .. result:upper()
+end, { expr = true })
+
+map("x", "p", "P", opts)
+map("x", "P", "p", opts)
+-- don't store blank lines in registers
+map("x", "d", function()
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+  local lines = vim.fn.getline(start_line, end_line)
+  if type(lines) == "string" then
+    lines = { lines }
+  end
+  local all_blank = true
+  for _, line in ipairs(lines) do
+    if not line:match("^%s*$") then
+      all_blank = false
+      break
+    end
+  end
+  if all_blank then
+    vim.cmd('normal! "_d')
+  else
+    vim.cmd("normal! d")
+  end
+end, opts)
--- lua/utils.lua
+++ lua/utils.lua
@@ -9,33 +9,3 @@ function TableConcat(...)
 
   return result
 end
-
-function DeleteLine()
-  local line = vim.api.nvim_get_current_line()
-  if string.match(line, "^%s*$") then
-    vim.cmd('normal! "_dd')
-  else
-    vim.cmd("normal! dd")
-  end
-end
-
-function DeleteBlock()
-  local start_line = vim.fn.line("'<")
-  local end_line = vim.fn.line("'>")
-  local lines = vim.fn.getline(start_line, end_line)
-  if type(lines) == "string" then
-    lines = { lines }
-  end
-  local all_blank = true
-  for _, line in ipairs(lines) do
-    if not line:match("^%s*$") then
-      all_blank = false
-      break
-    end
-  end
-  if all_blank then
-    vim.cmd('normal! "_d')
-  else
-    vim.cmd("normal! d")
-  end
-end
