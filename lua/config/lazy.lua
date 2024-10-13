50e3c0bbe9b7cce8252049e3835b29ce7880cdf4
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-13 19:29:14 +0900

    fix: remove default comment

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -59,13 +59,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.vscode" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = true,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight" } },
   checker = {
@@ -74,12 +69,8 @@ require("lazy").setup({
   }, -- automatically check for plugin updates
   performance = {
     rtp = {
-      -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
