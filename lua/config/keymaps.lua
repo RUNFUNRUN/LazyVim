50e92f62d1e63beaead5b03287e9aae177d1c50c
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2024-10-13 18:00:19 +0900

    fix: add terminal border

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,13 +1,22 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local util = require("lazyvim.util")
+
 local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
+
+local lazyterm = function()
+  util.terminal(nil, { cwd = util.root(), border = "rounded" })
+end
 
 map("n", "<Esc><Esc>", "<Cmd>nohlsearch<CR>", opts)
 map("n", "+", "<C-a>", opts)
 map("n", "-", "<C-x>", opts)
 map("n", "<C-a>", "gg<S-v>G", opts)
+map("n", "<leader>ft", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<C-/>", lazyterm, { desc = "Terminal (root dir)" })
+map("n", "<C-_>", lazyterm, { desc = "which_key_ignore" })
 -- don't store a blank line in registers
 map("n", "dd", function()
   local line = vim.api.nvim_get_current_line()
