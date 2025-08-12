dd750783d8fa555b679d0108172c601c3b5bc82f
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2025-08-12 21:52:24 +0900

    fix: remove github copilot

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,8 +25,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
-    { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.coding.mini-comment" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
